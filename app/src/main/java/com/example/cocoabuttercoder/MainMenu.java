4ed592fff6c72bf4d46a50081798c6c260f47a24
Bryan <cocoabuttercoder@gmail.com>
2019-03-21 11:35:12 -0400

    Added and styled social media icons

--- app/src/main/java/com/example/cocoabuttercoder/MainMenu.java
+++ app/src/main/java/com/example/cocoabuttercoder/MainMenu.java
@@ -1,7 +1,9 @@
 package com.example.cocoabuttercoder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+    }
+
+    public void openSocialMedia(View view){
+        startActivity(new Intent(MainMenu.this, SocialMedia.class));
     }
 }
