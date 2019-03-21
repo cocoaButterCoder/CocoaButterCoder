793b2ebf4f98a19af9691bc2045bb87f8ca2b259
Bryan <cocoabuttercoder@gmail.com>
2019-03-21 10:58:05 -0400

    Added social media activity

---
+++ app/src/main/java/com/example/cocoabuttercoder/SocialMedia.java
@@ -0,0 +1,13 @@
+package com.example.cocoabuttercoder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SocialMedia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.social_media);
+    }
+}
